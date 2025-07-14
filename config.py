f767cc9e55966907e3b2008612a2a2386bbeefbe
hokagelegend9999 <158546743+hokagelegend9999@users.noreply.github.com>
2025-07-14 12:43:06 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+# Ganti dengan Token Bot Anda
+BOT_TOKEN = '8118023652:AAEYgQMJpYrzqs3xCyyn_qaqtYxfas6TC6s'
+
+# Ganti dengan Telegram ID Anda (sebagai Admin Utama)
+# Cari tahu ID Anda dengan mengirim pesan ke @userinfobot
+ADMIN_TELEGRAM_ID = 1469244768
+
+# Pengaturan Harga
+SSH_PRICE = 1000  # Contoh harga dalam Rupiah (tanpa titik)
